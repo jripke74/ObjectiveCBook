d67972f3793a16f8429a0f4decfe00affb7509c6
Jeff Ripke <jripke74@gmail.com>
2017-08-09 12:15:20 -0700

    passed by reference

--- PassByReference/PassByReference/main.c
+++ PassByReference/PassByReference/main.c
@@ -6,3 +6,17 @@
 //  Copyright © 2017 Jeff Ripke. All rights reserved.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    double pi = 3.14;
+    double integerPart;
+    double fractionPart;
+    // Pass the address of integerPart as an argument
+    fractionPart = modf(pi, &integerPart);
+    // Find the value stored in integerPart
+    printf("integerPart = %.0f, fractionPart = %.2f\n", integerPart, fractionPart);
+    return EXIT_SUCCESS;
+}
