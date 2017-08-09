b1dc20a0f24793a07211506ed7a3171543509869
Jeff Ripke <jripke74@gmail.com>
2017-08-09 11:30:22 -0700

    add size of float

--- HowMuchMemoryChallenge/HowMuchMemoryChallenge/main.c
+++ HowMuchMemoryChallenge/HowMuchMemoryChallenge/main.c
@@ -6,10 +6,11 @@
 //  Copyright © 2017 Jeff Ripke. All rights reserved.
 //
 
-#include <stdio.h>
+#import <stdio.h>
+#import <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, const char * argn[]) {
+    float i = 3.2;
+    printf("A float is %zu bytes.\n", sizeof(i));
+    return EXIT_SUCCESS;
 }
