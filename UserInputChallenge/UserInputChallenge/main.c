cbb4eb6edb2fbb1c1be357ac65fcf9369927accb
Jeff Ripke <jripke74@gmail.com>
2017-08-09 10:20:53 -0700

    count down challenge done

--- UserInputChallenge/UserInputChallenge/main.c
+++ UserInputChallenge/UserInputChallenge/main.c
@@ -6,10 +6,21 @@
 //  Copyright © 2017 Jeff Ripke. All rights reserved.
 //
 
-#include <stdio.h>
+#import <stdio.h>
+#import <stdlib.h>
+#import <readline/readline.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, const char * argn[]) {
+    printf("Where should I start counting? ");
+    const char *numberString = readline(NULL);
+    int number = atoi(numberString);
+    int i;
+    for (i = number; i > 0; i -= 3) {
+        if (i % 5 == 0) {
+            printf("Found one!\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    return EXIT_SUCCESS;
 }
