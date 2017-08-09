4ef2f4e6f880309ff48a5f4187c6a732755ce337
Jeff Ripke <jripke74@gmail.com>
2017-08-09 10:35:54 -0700

    add Addresses project in c

---
+++ Addresses/Addresses/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Addresses
+//
+//  Created by Jeff Ripke on 8/9/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
