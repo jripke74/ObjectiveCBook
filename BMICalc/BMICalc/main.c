d244b6e036a5d4db17f1035063f8c71fc7105010
Jeff Ripke <jripke74@gmail.com>
2017-08-10 10:56:48 -0700

    add struct to BMICalc project

--- BMICalc/BMICalc/main.c
+++ BMICalc/BMICalc/main.c
@@ -10,7 +10,24 @@
 #include <stdlib.h>
 #include <math.h>
 
+struct Person {
+    float heightInMeters;
+    int weightInKilos;
+};
+
 int main(int argc, const char * argv[]) {
+    struct Person mikey;
+    mikey.heightInMeters = 1.97;
+    mikey.weightInKilos = 110;
+    
+    struct Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("mikey is %.2f meters tall\n", mikey.heightInMeters);
+    printf("mikey weighs %d kilograms\n", mikey.weightInKilos);
+    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
     
     return EXIT_SUCCESS;
 }
