e9f13308e6c9a8831ddacad9823ceb8fd9372eee
Jeff Ripke <jripke74@gmail.com>
2017-08-10 12:08:57 -0700

    add NSDate reference to TimeAfterTime project

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -9,7 +9,10 @@
 #import <Foundation/Foundation.h>
 
 int main (int argc, const char * argv[]) {
-    
+    @autoreleasepool {
+        NSDate *now = [NSDate date];
+        NSLog(@"This NSDate object lives at %p", now);
+    }
     
     return EXIT_SUCCESS;
 }
