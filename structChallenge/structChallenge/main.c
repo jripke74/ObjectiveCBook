b30bb64ca160a53c5c440450cbca171f9587b3e0
Jeff Ripke <jripke74@gmail.com>
2017-08-10 11:26:40 -0700

    completed struct challenge

--- structChallenge/structChallenge/main.c
+++ structChallenge/structChallenge/main.c
@@ -8,8 +8,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
+    long secondsSince1970 = time(NULL);
+    secondsSince1970 += 4000000;
+    struct tm fourMillionSecondFromNow;
+    localtime_r(&secondsSince1970, &fourMillionSecondFromNow);
+    printf("The time four million seconds from now is %d-%d-%d\n", fourMillionSecondFromNow.tm_mon, fourMillionSecondFromNow.tm_mday, fourMillionSecondFromNow.tm_year + 1900);
     
     return EXIT_SUCCESS;
 }
