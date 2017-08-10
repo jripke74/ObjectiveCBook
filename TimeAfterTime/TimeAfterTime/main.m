4b37c41fe95a6a71774c8ec15d6e785143b51c30
Jeff Ripke <jripke74@gmail.com>
2017-08-10 12:01:06 -0700

    add TimeAfterTime project

---
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Jeff Ripke on 8/10/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    
+    
+    return EXIT_SUCCESS;
+}
