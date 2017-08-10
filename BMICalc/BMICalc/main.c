2a7bc81bdc1345365f4d3223f5efcb20c671c0db
Jeff Ripke <jripke74@gmail.com>
2017-08-10 10:47:04 -0700

    add BMICalc project

---
+++ BMICalc/BMICalc/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by Jeff Ripke on 8/10/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return EXIT_SUCCESS;
+}
