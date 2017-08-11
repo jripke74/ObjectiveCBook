c60e3114a0084672e7e8b94d953e998751836ed8
Jeff Ripke <jripke74@gmail.com>
2017-08-11 11:46:25 -0700

    add TimesTwo project

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Jeff Ripke on 8/11/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
