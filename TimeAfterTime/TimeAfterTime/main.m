ef9a6a124f3f677c4c2c233ba7fc923fec9cabe4
Jeff Ripke <jripke74@gmail.com>
2017-08-11 10:13:40 -0700

    used alloc mehtod for NSDate

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -10,7 +10,9 @@
 
 int main (int argc, const char * argv[]) {
     @autoreleasepool {
-        NSDate *now = [NSDate date];
+        // This is the convience method to the alloc mothed.
+        //NSDate *now = [NSDate date];
+        NSDate *now = [[NSDate alloc] init];
         NSLog(@"This NSDate object lives at %p", now);
         NSLog(@"The date is %@", now);
         
@@ -22,7 +24,9 @@ int main (int argc, const char * argv[]) {
         
         NSCalendar *cal = [NSCalendar currentCalendar];
         NSLog(@"My calendar is %@", [cal calendarIdentifier]);
-        unsigned long day = [cal ordinalityOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:now];
+        unsigned long day = [cal ordinalityOfUnit:NSCalendarUnitDay
+                                           inUnit:NSCalendarUnitMonth
+                                          forDate:now];
         NSLog(@"This is day %lu of the month", day);
     }
     return EXIT_SUCCESS;
