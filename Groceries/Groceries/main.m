7c86fdd39d107026b1af4576941e2e51f363acf5
Jeff Ripke <jripke74@gmail.com>
2017-08-14 13:02:00 -0700

    add Groceries challenge project

---
+++ Groceries/Groceries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Groceries
+//
+//  Created by Jeff Ripke on 8/14/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
