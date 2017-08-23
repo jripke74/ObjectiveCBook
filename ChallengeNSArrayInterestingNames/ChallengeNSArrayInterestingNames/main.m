8318e721811763cd16f5bf5595d9265753d0390f
Jeff Ripke <jripke74@gmail.com>
2017-08-23 02:42:25 -0700

    completed ChallengeNSArrayInterestingNames

--- ChallengeNSArrayInterestingNames/ChallengeNSArrayInterestingNames/main.m
+++ ChallengeNSArrayInterestingNames/ChallengeNSArrayInterestingNames/main.m
@@ -13,19 +13,17 @@ int main(int argc, const char * argv[]) {
         
         // Read in a file as a huge string (ignoring the posibility of an error)
         NSString *nameString = [NSString stringWithContentsOfFile:@"/usr/share/dict/propernames" encoding:NSUTF8StringEncoding error:NULL];
-        
+        NSString *nameWordString = [NSString stringWithContentsOfFile:@"/usr/share/dict/words" encoding:NSUTF8StringEncoding error:NULL];
         // Break it into an array of strings
         NSArray *names = [nameString componentsSeparatedByString:@"\n"];
+        NSArray *words = [nameWordString componentsSeparatedByString:@"\n"];
         
         // Go through the array one string at a time
         for (NSString *n in names) {
-            
-            // Look for the string "aa" in a case-insensitive manner
-            NSRange r = [n rangeOfString:@"AA" options:NSCaseInsensitiveSearch];
-            
-            // Was it found?
-            if (r.location != NSNotFound) {
-                NSLog(@"%@", n);
+            for (NSString *w in words) {
+                if (n.lowercaseString == w) {
+                    NSLog(@"%@ = %@", n, w);
+                }
             }
         }
     }
