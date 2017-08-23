68d6a4b37c7ebb149bf704846d04369e89818346
Jeff Ripke <jripke74@gmail.com>
2017-08-23 01:41:54 -0700

    Completed grocery list challenge for arrays

--- Groceries/Groceries/main.m
+++ Groceries/Groceries/main.m
@@ -10,7 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        NSMutableArray *groceryList = [NSMutableArray array];
+        [groceryList addObject:@"Loaf of bread"];
+        [groceryList addObject:@"Container of milk"];
+        [groceryList addObject:@"Stick of butter"];
+        NSLog(@"My grocery list is:");
+        for (NSString *item in groceryList) {
+            NSLog(@"%@", item);
+        }
     }
     return EXIT_SUCCESS;
 }
