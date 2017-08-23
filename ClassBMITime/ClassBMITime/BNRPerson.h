02e5e2ea8b13135eb2873bfe0f1b5a69aab0907a
Jeff Ripke <jripke74@gmail.com>
2017-08-23 12:17:07 -0700

    add BNRPerson class and two instance variables

---
+++ ClassBMITime/ClassBMITime/BNRPerson.h
@@ -0,0 +1,19 @@
+//
+//  BNRPerson.h
+//  ClassBMITime
+//
+//  Created by Jeff Ripke on 8/23/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRPerson : NSObject
+
+// BNPerson has two instance variables
+{
+    float _heightInMeters;
+    int _weightInKilos;
+}
+
+@end
---
+++ ClassBMITime/ClassBMITime/BNRPerson.m
@@ -0,0 +1,13 @@
+//
+//  BNRPerson.m
+//  ClassBMITime
+//
+//  Created by Jeff Ripke on 8/23/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import "BNRPerson.h"
+
+@implementation BNRPerson
+
+@end
