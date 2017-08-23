389496d7a76b3b7a534e89ea4add1cdeb8e2efd5
Jeff Ripke <jripke74@gmail.com>
2017-08-23 12:08:57 -0700

    add ClassBMITime project

---
+++ ClassBMITime/ClassBMITime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClassBMITime
+//
+//  Created by Jeff Ripke on 8/23/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS
+}
