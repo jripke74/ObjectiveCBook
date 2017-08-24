584f5b45d9abd96df0b3b03ee9a93ad53a901080
Jeff Ripke <jripke74@gmail.com>
2017-08-24 00:45:51 -0700

    add ChatllengeClassStocks project

---
+++ ChallengeClassStocks/ChallengeClassStocks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChallengeClassStocks
+//
+//  Created by Jeff Ripke on 8/24/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
