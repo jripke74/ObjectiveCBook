6ac8815a08a67b84b6985bff950146aafa02efb1
Jeff Ripke <jripke74@gmail.com>
2017-08-24 10:14:19 -0700

    update JARStockHolding.m

--- ChallengeClassStocks/JARStockHolding.m
+++ ChallengeClassStocks/JARStockHolding.m
@@ -10,6 +10,39 @@
 
 @implementation JARStockHolding
 
+- (float)purchaseSharePrice {
+    return _purchaseSharePrice;
+}
 
+- (void)setPurchaseSharePrice:(float)purchasePrice {
+    _purchaseSharePrice = purchasePrice;
+}
 
+- (float)currentSharePrice {
+    return _currentSharePrice;
+}
+
+- (void)setCurrentSharePrice:(float)currentPrice {
+    _currentSharePrice = currentPrice;
+}
+
+- (int)numberOfShares {
+    return _numberOfShares;
+}
+
+- (void)setNumberOfShares:(int)numberShares {
+    _numberOfShares = numberShares;
+}
+
+// calculate the total cost of shares when purchased
+- (float)costInDollars {
+    float purchasePrice = [self purchaseSharePrice];
+    return [self numberOfShares] * purchasePrice;
+}
+
+// calculates the value of your total share on the current price per share
+- (float)valueInDollars {
+    float currentPrice = [self currentSharePrice];
+    return [self numberOfShares] * currentPrice;
+}
 @end
