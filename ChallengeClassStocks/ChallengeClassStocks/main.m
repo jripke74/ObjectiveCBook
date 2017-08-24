98c107c8be69c613ee1267d153a192fd4e2bdbb6
Jeff Ripke <jripke74@gmail.com>
2017-08-24 10:33:35 -0700

    completed class challenge ChallengeClassStocks project

--- ChallengeClassStocks/ChallengeClassStocks/main.m
+++ ChallengeClassStocks/ChallengeClassStocks/main.m
@@ -18,8 +18,28 @@ int main(int argc, const char * argv[]) {
         JARStockHolding *stockThree = [[JARStockHolding alloc] init];
         
         // Give the instance variables values using setters
+        [stockOne setPurchaseSharePrice:1.2];
+        [stockOne setCurrentSharePrice:2.22];
+        [stockOne setNumberOfShares:30];
+        [stockTwo setPurchaseSharePrice:44.4];
+        [stockTwo setCurrentSharePrice:84.2];
+        [stockTwo setNumberOfShares:12];
+        [stockThree setPurchaseSharePrice:29.0];
+        [stockThree setCurrentSharePrice:120.3];
+        [stockThree setNumberOfShares:40];
         
+        // create empty mutable array called stocks
+        NSMutableArray *stocks = [NSMutableArray array];
         
+        // add stocks to array
+        [stocks addObject:stockOne];
+        [stocks addObject:stockTwo];
+        [stocks addObject:stockThree];
+        
+        // iterate throgh the stocks array, logging each stock
+        for (JARStockHolding *stock in stocks) {
+            NSLog(@"Curent stock value: $%.2f", stock.valueInDollars);
+        }
     }
     return EXIT_SUCCESS;
 }
