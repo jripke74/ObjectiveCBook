76b05d1634a4985b5434b0b77d9274315ae0e53e
Jeff Ripke <jripke74@gmail.com>
2017-08-29 10:17:00 -0700

    add employee details

--- ClassBMITime/ClassBMITime/main.m
+++ ClassBMITime/ClassBMITime/main.m
@@ -15,13 +15,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // Create an instance of CBTEmployee
-        CBTPerson *mikey = [[CBTEmployee alloc] init];
+        CBTEmployee *mikey = [[CBTEmployee alloc] init];
         
         // Give the instance variables interesting values using setters
 //        [mikey setWeightInKilos:96];
 //        [mikey setHeightInMeters:1.8];
         mikey.weightInKilos = 96;
         mikey.heightInMeters = 1.8;
+        mikey.employeeID = 12;
+        mikey.hireDate = [NSDate dateWithNaturalLanguageString:@"Aug 2nd, 2010"];
         
         // Log the instance variables using the getters
 //        float height = [mikey heightInMeters];
@@ -29,10 +31,12 @@ int main(int argc, const char * argv[]) {
         float height = mikey.heightInMeters;
         int weight = mikey.weightInKilos;
         NSLog(@"mikey is %.2f meters tall and weighs %d kilograms", height, weight);
+        NSLog(@"Emnployee %u hired on %@", mikey.employeeID, mikey.hireDate);
         
         // Log some values using custom methods
         float bmi = [mikey bodyMassIndex];
-        NSLog(@"mikey has a BMI of %f", bmi);
+        double years = [mikey yearsOfEmployment];
+        NSLog(@"BMI of %.2f, has worked with us for %.2f years", bmi, years);
         
     }
     return EXIT_SUCCESS;
