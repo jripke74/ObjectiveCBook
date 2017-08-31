f707d891a801af96979b420271d3b1d754eea355
Jeff Ripke <jripke74@gmail.com>
2017-08-31 10:31:42 -0700

    CLassBMITime: add CBTAsset and two @property

---
+++ ClassBMITime/ClassBMITime/CBTAsset.h
@@ -0,0 +1,16 @@
+//
+//  CBTAsset.h
+//  ClassBMITime
+//
+//  Created by Jeff Ripke on 8/31/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBTAsset : NSObject
+
+@property (nonatomic, copy) NSString *label;
+@property (nonatomic) unsigned int resetleValue;
+
+@end
