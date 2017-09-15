04fa193a72ba948e170bd1ef991e383b81ce4d6c
Jeff Ripke <jripke74@gmail.com>
2017-09-15 09:50:53 -0700

    Constants: global variables

--- Constants/Constants/main.m
+++ Constants/Constants/main.m
@@ -12,6 +12,11 @@ int main (int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"\u03c0 is %f", M_PI);
         NSLog(@"%d is larger", MAX(10, 12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        // you can use the string @"currency" use constant NSLocaleCurrencyCode instead
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
     }
     return EXIT_SUCCESS;
 }
