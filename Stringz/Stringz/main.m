d638e40093f8de481d9b823ccbaef81b1469c500
Jeff Ripke <jripke74@gmail.com>
2017-09-15 11:07:49 -0700

    Stringz: reading from file with NSString

--- Stringz/Stringz/main.m
+++ Stringz/Stringz/main.m
@@ -11,25 +11,33 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        NSMutableString *str = [[NSMutableString alloc] init];
-        for (int i = 0; i < 10; i++) {
-            [str appendString:@"Jeff is cool!\n"];
-        }
+//        NSMutableString *str = [[NSMutableString alloc] init];
+//        for (int i = 0; i < 10; i++) {
+//            [str appendString:@"Jeff is cool!\n"];
+//        }
+//        
+//        // Declare a pointer to an NSError object, but do not instantiate it
+//        // The NSError instance will only be created if there is, in fact, an error.
+//        NSError *error;
+//        
+//        // Pass the NSEoror pointer by reference to the NSString method
+//        BOOL success = [str writeToFile:@"/tmph/cool.txt" atomically: YES encoding: NSUTF8StringEncoding error: &error];
+//        
+//        // Test the returned BOOL, and query the NSError if the write failed
+//        if (success) {
+//            NSLog(@"done writing /tmp/cool.txt");
+//        } else {
+//            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+//        }
         
-        // Declare a pointer to an NSError object, but do not instantiate it
-        // The NSError instance will only be created if there is, in fact, an error.
         NSError *error;
+        NSString *str = [[NSString alloc] initWithContentsOfFile:@"/etc/resolv.conf" encoding: NSASCIIStringEncoding error:&error];
         
-        // Pass the NSEoror pointer by reference to the NSString method
-        BOOL success = [str writeToFile:@"/tmph/cool.txt" atomically: YES encoding: NSUTF8StringEncoding error: &error];
-        
-        // Test the returned BOOL, and query the NSError if the write failed
-        if (success) {
-            NSLog(@"done writing /tmp/cool.txt");
+        if (!str) {
+            NSLog(@"read failed: %@", [error localizedDescription]);
         } else {
-            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+            NSLog(@"resolv.conf looks like this: %@", str);
         }
-        
     }
     return EXIT_SUCCESS;
 }
