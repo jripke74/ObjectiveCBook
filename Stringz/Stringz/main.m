b06c96f5ffee33999d364824cc738f4307ea88aa
Jeff Ripke <jripke74@gmail.com>
2017-09-15 10:01:56 -0700

    Stringz: writing to files with NSString

---
+++ Stringz/Stringz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by Jeff Ripke on 9/15/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
