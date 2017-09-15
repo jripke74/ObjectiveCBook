45931e999fb2c603c6a82e1194d679c093948e39
Jeff Ripke <jripke74@gmail.com>
2017-09-15 09:43:52 -0700

    Constants: #define MAX

--- Constants/Constants/main.m
+++ Constants/Constants/main.m
@@ -11,6 +11,7 @@
 int main (int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 12));
     }
     return EXIT_SUCCESS;
 }
