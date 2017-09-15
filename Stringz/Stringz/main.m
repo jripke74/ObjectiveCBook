864fa555edc3a55837c4d94629ba19ede130e220
Jeff Ripke <jripke74@gmail.com>
2017-09-15 10:19:08 -0700

    Stringz: write an NSString to a file named cool.txt

--- Stringz/Stringz/main.m
+++ Stringz/Stringz/main.m
@@ -10,7 +10,12 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        NSMutableString *str = [[NSMutableString alloc] init];
+        for (int i = 0; i < 10; i++) {
+            [str appendString:@"Jeff is cool!\n"];
+        }
+        [str writeToFile:@"/tmp/cool.txt" atomically: YES encoding: NSUTF8StringEncoding error: NULL];
+        NSLog(@"done writing /tmp/cool.txt");
     }
     return EXIT_SUCCESS;
 }
