5a8dc8702f0d956fb9738aac368ca09921238167
Jeff Ripke <jripke74@gmail.com>
2017-09-15 10:28:26 -0700

    Stringz: use NSError to check for errors

--- Stringz/Stringz/main.m
+++ Stringz/Stringz/main.m
@@ -10,12 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
         NSMutableString *str = [[NSMutableString alloc] init];
         for (int i = 0; i < 10; i++) {
             [str appendString:@"Jeff is cool!\n"];
         }
-        [str writeToFile:@"/tmp/cool.txt" atomically: YES encoding: NSUTF8StringEncoding error: NULL];
-        NSLog(@"done writing /tmp/cool.txt");
+        
+        // Declare a pointer to an NSError object, but do not instantiate it
+        // The NSError instance will only be created if there is, in fact, an error.
+        NSError *error;
+        
+        // Pass the NSEoror pointer by reference to the NSString method
+        BOOL success = [str writeToFile:@"/tmph/cool.txt" atomically: YES encoding: NSUTF8StringEncoding error: &error];
+        
+        // Test the returned BOOL, and query the NSError if the write failed
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
+        
     }
     return EXIT_SUCCESS;
 }
