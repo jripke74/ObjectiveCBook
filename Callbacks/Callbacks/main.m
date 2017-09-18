4f5e760778ca8412b62c4254bed7b4d562540d10
Jeff Ripke <jripke74@gmail.com>
2017-09-18 11:06:17 -0700

    Callbacks: add Callbacks project

---
+++ Callbacks/Callbacks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Jeff Ripke on 9/18/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
