3a14ee18276c2d616be1cf3d1bacab1e32f6d1c2
Jeff Ripke <jripke74@gmail.com>
2017-10-03 11:33:32 -0700

    VowelMovement: created project

---
+++ VowelMovement/VowelMovement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Jeff Ripke on 10/3/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * arg[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
