88d9bce36fed1ee7426fdb6b28b5489acfc5ed37
Jeff Ripke <jripke74@gmail.com>
2017-10-03 11:39:42 -0700

    VowelMovement: add 3 arrays

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -10,7 +10,12 @@
 
 int main(int argc, const char * arg[]) {
     @autoreleasepool {
-        
+        // create array of strings and a container for devowelized ones
+        NSArray *originalStrings = @[@"Sauerkraut", @"Raygun", @"Big Nerd Ranch", @"Mississippi"];
+        NSLog(@"original strings: %@", originalStrings);
+        NSMutableArray *dovowelizedStrings = [NSMutableArray array];
+        // create a list of characters to be removed from the string
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
     }
     return EXIT_SUCCESS;
 }
