dd14e6044186f3e357a3d4a0d8210713875caa25
Jeff Ripke <jripke74@gmail.com>
2017-10-04 13:17:00 -0700

    Stockz: initial commit

---
+++ Stockz/Stockz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Jeff Ripke on 10/4/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return EXIT_SUCCESS;
+}
