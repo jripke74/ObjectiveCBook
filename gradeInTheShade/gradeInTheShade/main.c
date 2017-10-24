89f6f6d4eac09f57d8c812c5411f4a6dfccbbe4d
Jeff Ripke <jripke74@gmail.com>
2017-10-24 12:21:45 -0700

    gradeInTheShade: removed malloc()

--- gradeInTheShade/gradeInTheShade/main.c
+++ gradeInTheShade/gradeInTheShade/main.c
@@ -7,7 +7,6 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h> // malloc(), free()
 
 float averageFloats(float *data, int dataCount) {
     float sum = 0.0;
@@ -18,15 +17,13 @@ float averageFloats(float *data, int dataCount) {
 }
 
 int main(int argc, const char * argv[]) {
-    float *gradeBook = malloc(3 * sizeof(float));
+    float gradeBook[3];
     gradeBook[0] = 60.2;
     gradeBook[1] = 94.5;
     gradeBook[2] = 81.1;
     
     float average = averageFloats(gradeBook, 3);
     
-    free(gradeBook);
-    gradeBook = NULL;
     printf("Average = %.2f\n", average);
     
     return 0;
